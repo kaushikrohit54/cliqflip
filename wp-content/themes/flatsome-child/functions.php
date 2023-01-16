ab7a345da60610a6441f5868cf43e57ed0b4b725
kaushikrohit54 <kaushikrohit54@gmail.com>
2023-01-16 16:19:08 +0530

    additional tab merged

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -46,3 +46,23 @@ function cliqflip_buy_now()
 
 add_action( 'wp_loaded', 'cliqflip_buy_now' );
 /* Dynamic Button for Simple & Variable Product Closed */
+// Remove additional information tab
+
+add_filter( 'woocommerce_product_tabs', function( $tabs ) {
+	unset( $tabs['additional_information'] );
+	return $tabs;
+}, 98 );
+
+// Insert additional information into description tab
+
+add_filter( 'woocommerce_product_tabs', function( $tabs ) {
+	$tabs['description']['callback'] = function() {
+		global $product;
+		
+		if ( $product && ( $product->has_attributes() || apply_filters( 'wc_product_enable_dimensions_display', $product->has_weight() || $product->has_dimensions() ) ) ) {
+			wc_get_template( 'single-product/tabs/additional-information.php' );
+		}
+        wc_get_template( 'single-product/tabs/description.php' );
+	};
+	return $tabs;
+}, 98 );
