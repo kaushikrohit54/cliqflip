4b209b186e8a9240a9fa63cf9af3aa63a4929b41
kaushikrohit54 <kaushikrohit54@gmail.com>
2023-01-19 18:14:49 +0530

    home menu added files deleted

--- wp-content/themes/flatsome/inc/woocommerce/structure-wc-product-page.php
+++ wp-content/themes/flatsome/inc/woocommerce/structure-wc-product-page.php
@@ -251,19 +251,19 @@ function flatsome_sticky_add_to_cart_before() {
 
 	echo '<div class="sticky-add-to-cart-wrapper">';
 	echo '<div class="sticky-add-to-cart">';
-	echo '<div class="sticky-add-to-cart__product">';
-	$image_id = $product->get_image_id();
-	$image    = wp_get_attachment_image_src( $image_id, 'woocommerce_gallery_thumbnail' );
-	if ( $image ) {
-		$image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
-		$image     = '<img src="' . $image[0] . '" alt="' . $image_alt . '" class="sticky-add-to-cart-img" />';
-		echo $image;
-	}
-	echo '<div class="product-title-small hide-for-small"><strong>' . get_the_title() . '</strong></div>';
-	if ( ! $product->is_type( 'variable' ) ) {
-		woocommerce_template_single_price();
-	}
-	echo '</div>';
+	// echo '<div class="sticky-add-to-cart__product">';
+	// $image_id = $product->get_image_id();
+	// $image    = wp_get_attachment_image_src( $image_id, 'woocommerce_gallery_thumbnail' );
+	// if ( $image ) {
+	// 	$image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+	// 	$image     = '<img src="' . $image[0] . '" alt="' . $image_alt . '" class="sticky-add-to-cart-img" />';
+	// 	echo $image;
+	// }
+	// echo '<div class="product-title-small hide-for-small"><strong>' . get_the_title() . '</strong></div>';
+	// if ( ! $product->is_type( 'variable' ) ) {
+	// 	woocommerce_template_single_price();
+	// }
+	// echo '</div>';
 }
 
 add_action( 'woocommerce_before_add_to_cart_button', 'flatsome_sticky_add_to_cart_before', -100 );
