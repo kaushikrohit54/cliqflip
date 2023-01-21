e382a1de87036b417226dbc7563bcc2eab8a094b
kaushikrohit54 <kaushikrohit54@gmail.com>
2023-01-21 18:08:23 +0530

    description corrected

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -58,11 +58,11 @@ add_filter( 'woocommerce_product_tabs', function( $tabs ) {
 add_filter( 'woocommerce_product_tabs', function( $tabs ) {
 	$tabs['description']['callback'] = function() {
 		global $product;
-		
+        wc_get_template( 'single-product/tabs/description.php' );
 		if ( $product && ( $product->has_attributes() || apply_filters( 'wc_product_enable_dimensions_display', $product->has_weight() || $product->has_dimensions() ) ) ) {
 			wc_get_template( 'single-product/tabs/additional-information.php' );
 		}
-        wc_get_template( 'single-product/tabs/description.php' );
+       
 	};
 	return $tabs;
 }, 98 );
